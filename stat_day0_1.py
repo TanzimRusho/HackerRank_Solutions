4b0ca3452576944c84d4ec17c7d91cd45b7cb0d6
Tanzim Ahmed <32319601+TanzimRusho@users.noreply.github.com>
2021-07-18 11:16:20 +0600

    Create stat_day0_1.py

---
+++ stat_day0_1.py
@@ -0,0 +1,31 @@
+# Hackerrank: Day 0: Weighted Mean
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'weightedMean' function below.
+#
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY X
+#  2. INTEGER_ARRAY W
+#
+
+def weightedMean(X, W):
+    # Write your code here
+    total = sum([i*j for i, j in zip(X, W)])    
+    print("{:.1f}".format(total / sum(W)))
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    vals = list(map(int, input().rstrip().split()))
+
+    weights = list(map(int, input().rstrip().split()))
+
+    weightedMean(vals, weights)
